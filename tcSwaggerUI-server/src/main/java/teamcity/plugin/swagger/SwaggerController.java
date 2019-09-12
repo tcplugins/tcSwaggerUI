811495226d10280d5e43bd882c69d17e1151944e
Net Wolf UK <netwolfuk@gmail.com>
2019-09-13 11:30:12 +1200

    tcSwaggerUI Plugin - Initial commit.

---
+++ tcSwaggerUI-server/src/main/java/teamcity/plugin/swagger/SwaggerController.java
@@ -0,0 +1,34 @@
+package teamcity.plugin.swagger;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import jetbrains.buildServer.controllers.BaseController;
+import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import jetbrains.buildServer.web.openapi.WebControllerManager;
+
+public class SwaggerController extends BaseController {
+	
+	private PluginDescriptor myPluginDescriptor;
+
+	public SwaggerController(
+			SBuildServer server, 
+			WebControllerManager webControllerManager,
+    		PluginDescriptor pluginDescriptor) {
+		super(server);
+		myPluginDescriptor = pluginDescriptor;
+		webControllerManager.registerController("/swagger-ui.html", this);
+	}
+
+	@Override
+	protected ModelAndView doHandle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HashMap<String,Object> params = new HashMap<>();		
+		params.put("jspHome", myPluginDescriptor.getPluginResourcesPath());
+		return new ModelAndView(myPluginDescriptor.getPluginResourcesPath() + "swagger-ui-wrapper.jsp", params);
+	}
+}
---
+++ tcSwaggerUI-server/src/main/resources/buildServerResources/swagger-ui-wrapper.jsp
@@ -0,0 +1,3 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<%@ include file="swagger-ui-3.0.21/dist/index.html" %>
